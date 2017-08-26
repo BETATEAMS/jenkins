d1dc1f3c3dca55784a3e8ceb039ba424eb1fd7bb
BETATEAMS <30910960+BETATEAMS@users.noreply.github.com>
2017-08-26 14:57:47 -0500

    Update HolaControllerTest.java

--- src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
+++ src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
@@ -1,4 +1,41 @@
-package es.dmunozfer.jenkins;
+package com.tutoriales.seleniumgrid;
+ 
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+ 
+import junit.framework.Assert;
+ 
+public class AdictosAlTrabajoTestCase {
+	
+	private Selenium selenium;
+ 
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("13.90.82.96", 4444, "*firefox", "http://www.adictosaltrabajo.com/");
+		selenium.start();
+	}
+ 
+	@Test
+	public void findTutorial() throws Exception {
+		selenium.open("/");
+		selenium.type("sbi", "selenium remote control");
+		selenium.click("sb");
+		selenium.waitForPageToLoad("30000");
+		
+		Assert.assertEquals("Adictos al Trabajo. Formación y desarrollo | JAVA, JEE, UML, XML |. Tutoriales sobre nuevas tecnologías.", selenium.getTitle());
+		selenium.selectFrame("googleSearchFrame");
+		selenium.click("//div[@id='res']/div[1]/ol/li[1]/div/h2/a/em");
+		selenium.waitForPageToLoad("30000");
+	}
+ 
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
+/*package es.dmunozfer.jenkins;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
