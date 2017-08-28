338ca9eba2acb5762338495a14154e5645f1c272
BETATEAMS <30910960+BETATEAMS@users.noreply.github.com>
2017-08-28 14:59:13 -0500

    Update HolaControllerTest.java

--- src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
+++ src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
@@ -9,21 +9,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import junit.framework.Assert;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
-@WebAppConfiguration
+
 public class HolaControllerTest {
 
-    @Autowired
+  
 	private Selenium selenium;
     
     @Before
