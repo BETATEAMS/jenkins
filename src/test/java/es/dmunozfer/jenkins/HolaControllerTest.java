e1ef8e6c5e0635b524820512a19d34e52d3b138f
BETATEAMS <30910960+BETATEAMS@users.noreply.github.com>
2017-08-28 14:26:02 -0500

    Update HolaControllerTest.java

--- src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
+++ src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
@@ -1,53 +1,61 @@
-#!groovy
+package es.dmunozfer.jenkins;
 
-node {
-   // ------------------------------------
-   // -- ETAPA: Compilar
-   // ------------------------------------
-   stage 'Compilar'
-   
-   // -- Configura variables
-   echo 'Configurando variables'
-   def mvnHome = tool 'M3'
-   env.PATH = "${mvnHome}/bin:${env.PATH}"
-   echo "var mvnHome='${mvnHome}'"
-   echo "var env.PATH='${env.PATH}'"
-   
-   // -- Descarga código desde SCM
-   echo 'Descargando código de SCM'
-   sh 'rm -rf *'
-   checkout scm
-   
-   // -- Compilando
-   echo 'Compilando aplicación'
-   sh 'mvn clean compile'
-   
-   // ------------------------------------
-   // -- ETAPA: Test
-   // ------------------------------------
-   stage 'Test'
-   echo 'Ejecutando tests'
-   try{
-      sh 'mvn verify'
-      step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-   }catch(err) {
-      step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-      if (currentBuild.result == 'UNSTABLE')
-         currentBuild.result = 'FAILURE'
-      throw err
-   }
-   
-   // ------------------------------------
-   // -- ETAPA: Instalar
-   // ------------------------------------
-   stage 'Instalar'
-   echo 'Instala el paquete generado en el repositorio maven'
-   sh 'mvn install -Dmaven.test.skip=true'
-   
-   // ------------------------------------
-   // -- ETAPA: Archivar
-   // ------------------------------------
-   stage 'Archivar'
-   echo 'Archiva el paquete el paquete generado en Jenkins'
-   step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar, **/target/*.war', fingerprint: true])
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemoApplication.class)
+@WebAppConfiguration
+public class HolaControllerTest {
+
+    @Autowired
+    private WebApplicationContext webContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc() {
+	mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+    }
+
+    @Test
+    public void raiz() throws Exception {
+	mockMvc.perform(get("/"))
+	.andExpect(status().is3xxRedirection())
+	.andExpect(header().string("Location", "/hola"));
+    }
+    
+    @Test
+    public void holaSinParametros() throws Exception {
+	mockMvc.perform(get("/hola"))
+	.andExpect(status().isOk())
+	.andExpect(view().name("hola"))
+	.andExpect(model().attributeExists("nombre"))
+	.andExpect(model().attribute("nombre", is("Mundo")))
+	.andExpect(content().string(containsString("¡Hola Mundo!")))
+	;
+    }
+    
+    @Test
+    public void holaDavid() throws Exception {
+	mockMvc.perform(get("/hola?nombre=David"))
+	.andExpect(status().isOk())
+	.andExpect(view().name("hola"))
+	.andExpect(model().attributeExists("nombre"))
+	.andExpect(model().attribute("nombre", is("David")))
+	.andExpect(content().string(containsString("¡Hola David!a")))
+	;
+    }
+
 }
