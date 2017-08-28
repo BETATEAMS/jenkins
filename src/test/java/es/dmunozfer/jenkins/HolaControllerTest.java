8e3cf979554126acddf94d277a521dff091578f0
BETATEAMS <30910960+BETATEAMS@users.noreply.github.com>
2017-08-28 18:43:55 -0500

    Update HolaControllerTest.java

--- src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
+++ src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
@@ -4,21 +4,37 @@ import com.thoughtworks.selenium.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+ 
+import java.util.Arrays;
+import java.util.Collection;
+ 
 import junit.framework.Assert;
-
-
+ 
+@RunWith(Parallelized.class)
 public class HolaControllerTest {
-
-  
+	
 	private Selenium selenium;
-    
-    @Before
-    public void setUp() throws Exception {
-		selenium = new DefaultSelenium("13.90.82.96", 4444, "*firefox", "http://www.adictosaltrabajo.com/");
+ 
+	private String browser;
+ 
+	public AdictosAlTrabajoTestCase(String browser) {
+	    this.browser = browser;
+	 }
+	
+	@Parameters
+	public static Collection getParameters(){
+		return Arrays.asList(new Object[][] { {"*firefox"} });
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("13.90.82.96", 4444, this.browser, "http://www.adictosaltrabajo.com/");
 		selenium.start();
 	}
-
-    @Test
+ 
+	@Test
 	public void findTutorial() throws Exception {
 		selenium.open("/");
 		selenium.type("sbi", "selenium remote control");
@@ -30,10 +46,9 @@ public class HolaControllerTest {
 		selenium.click("//div[@id='res']/div[1]/ol/li[1]/div/h2/a/em");
 		selenium.waitForPageToLoad("30000");
 	}
+ 
 	@After
 	public void tearDown() throws Exception {
 		selenium.stop();
 	}
- 
-
 }
