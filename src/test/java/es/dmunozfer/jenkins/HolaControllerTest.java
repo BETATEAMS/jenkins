8db9ce9ac731e785dbd35aa720a5d3ecbeec2896
BETATEAMS <30910960+BETATEAMS@users.noreply.github.com>
2017-08-28 19:56:43 -0500

    Update HolaControllerTest.java

--- src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
+++ src/test/java/es/dmunozfer/jenkins/HolaControllerTest.java
@@ -5,7 +5,7 @@ public class HolaControllerTest {
 
 public static void main (String[] args) {
 
-Selenium selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://www.b2mining.com/");
+Selenium selenium = new DefaultSelenium("13.90.82.96", 4444, "*firefox", "http://www.b2mining.com/");
 selenium.start();
 selenium.open("/");
 
